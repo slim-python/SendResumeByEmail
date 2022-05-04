403d7b096b9de37916070b6d3ad24644c032a961
Abhi Raj <xl4bhi@gmail.com>
2022-05-05 04:08:23 +0530

    Update AbhishekResumeRoute.js

--- routes/AbhishekResumeRoute.js
+++ routes/AbhishekResumeRoute.js
@@ -2,6 +2,6 @@ import express from "express";
 import { ShowResumeController } from "../controllers/ShowResumeController.js";
 
 const router = express.Router();
-router.get("/Full_stack_dev_abhishek", ShowResumeController);
+router.get("/Full_stack_dev_abhishek.pdf", ShowResumeController);
 
 export default router;
